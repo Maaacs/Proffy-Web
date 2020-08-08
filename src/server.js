a66aa896892477f323647e0888d807fdaaf7725c
Max Souza <maxbarbosa63@gmail.com>
2020-08-08 15:56:33 -0400

    Finalizando criação do servidor local

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
-require('express')()
-.get("/", (req, res) => {
-    return res.send("hi from")
+const express = require('express')
+const server = express()
 
+server.use(express.static("public"))
+.get("/", (req, res) => {
+    return res.sendFile(__dirname + "/views/index.html")
 })
 
 .get("/study", (req, res) => {
-        return res.send("Página Study")  
-
+        return res.sendFile(__dirname + "/views/study.html")  
 })
  
+.get("/give-classes", (req, res) => {
+    return res.sendFile(__dirname + "/views/give-classes.html")  
+})
+
 .listen(5500)
