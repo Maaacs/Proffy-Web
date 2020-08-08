a061c877dccf456d3ce4d4c9f4e0d3c0035c5d1c
Max Souza <maxbarbosa63@gmail.com>
2020-08-08 01:35:19 -0400

    Clonar e limpar novos campos de Novo Horário

--- scripts/addField.js
+++ scripts/addField.js
@@ -6,8 +6,18 @@ document.querySelector("#add-time")
 // Executar uma ação
 function cloneField(){
   //Duplicar campos. Mas que campos?
-  fields = document.querySelector('.schedule-item').cloneNode(true)
+  const newFieldContainer = document.querySelector('.schedule-item')
+  .cloneNode(true)
+
+  // limpar os novos campos: Que campos?
+  const fields = newFieldContainer.querySelectorAll('input')//para limpar os
+                                                        //campos no novo horário
+  //para cada campo novo, limpar
+  fields.forEach(function(field) {
+      //pega o field do momento e limpa ele {
+      field.value = ""
+  })
 
   // Colocar na página: onde?
-  document.querySelector('#schedule-items').appendChild(fields)
+  document.querySelector('#schedule-items').appendChild(newFieldContainer)
 }
